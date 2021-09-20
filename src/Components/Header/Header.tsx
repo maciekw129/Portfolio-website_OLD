0499a2620e88759ab9e74d8113882251171a4b29
Maciej Walecki <maciejwalecki99@gmail.com>
2021-09-20 21:34:30 +0200

    added global styles and router

---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import { 
+    HeaderContainer,
+    Title,
+ } from './style';
+
+export default function Header() {
+    return (
+        <div>
+            <HeaderContainer>
+                <Title>Maciej Walecki</Title>
+            </HeaderContainer>
+        </div>
+    )
+}
---
+++ src/Components/Header/style.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components/macro";
+
+export const HeaderContainer = styled.header`
+
+`;
+
+export const Title = styled.h1`
+
+`
