922366170f100bc0edc6a91d0b330278f8dcf20b
Maciej Walecki <maciejwalecki99@gmail.com>
2021-10-12 15:10:29 +0200

    added email form

--- src/Pages/CoctactMe/ContactMe.tsx
+++ src/Pages/CoctactMe/ContactMe.tsx
@@ -1,11 +1,50 @@
 import Page from '../../Components/Page/Page';
 import { contactMe } from '../../Utilities/languages';
 import contactMeHeroImage from '../../Images/contactMeHeroImage.svg';
+import React, { useState } from 'react';
+import emailjs from 'emailjs-com';
+import {
+    EmailForm,
+    EmailInput,
+    EmailText,
+    SendButton,
+} from './styles';
 
 const ContactMe: React.FC = () => {
+
+    const [ email, setEmail ] = useState('');
+    const [ message, setMessage ] = useState('');
+
+    const sendEmail = (e: React.ChangeEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        emailjs.sendForm('service_maciekw129', 'template_maciekw129', e.target, 'user_tjWm8B2iouokJPErZM8FG')
+        .then(response => {
+            console.log(response);
+            setEmail('');
+            setMessage(''); 
+        })
+    }
+
     return(
         <Page page={contactMe} character='!' image={contactMeHeroImage}>
-
+                <EmailForm onSubmit={sendEmail}>
+                    <label htmlFor="email">Your E-mail</label>
+                    <EmailInput 
+                        required
+                        type="email" 
+                        name="email" 
+                        value={email} 
+                        onChange={(e) => setEmail(e.target.value)} 
+                    />
+                    <label htmlFor="message">Message</label>
+                    <EmailText
+                        required
+                        name="message"
+                        value={message}
+                        onChange={(e) => setMessage(e.target.value)}
+                    />
+                    <SendButton type="submit">Send</SendButton>
+                 </EmailForm>
         </Page>
     )
 }
---
+++ src/Pages/CoctactMe/styles.tsx
@@ -0,0 +1,29 @@
+import styled from 'styled-components/macro';
+
+export const EmailForm = styled.form`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 2rem;
+`;
+
+export const EmailInput = styled.input`
+    width: 100%;
+    margin-bottom: 1rem;
+    border: 0;
+    background-color: hsl(0, 0%, 98%);
+    border-radius: 5px;
+`;
+
+export const EmailText = styled.textarea`
+    width: 100%;
+    margin-bottom: 1rem;
+    border: 0;
+    background-color: lightgray;
+    border-radius: 5px;
+`;
+
+export const SendButton = styled.button`
+
+`;
+    
